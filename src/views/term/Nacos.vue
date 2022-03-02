c268306f1021e39b534b240faf6b840710b6d9cb
john <tierney-john@hotmail.com>
2022-03-02 22:20:00 +0800

    新增控制台相关组件

--- src/views/term/Nacos.vue
+++ src/views/term/Nacos.vue
@@ -1,5 +1,48 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { outNacos, toNacos } from "@/api/out";
+
+onMounted(() => {
+  toNacos();
+});
+</script>
+
 <template>
- <h1>
-   Nacos
- </h1>
+  <div class="nacos">
+    <img
+      @click="outNacos()"
+      class="logo"
+      src="@/assets/image/term/nacos/nacos-logo.png"
+      alt="nacos"
+    />
+    <p class="title">
+      一个更易于构建云原生应用的动态服务发现、配置管理和服务管理平台。
+    </p>
+  </div>
 </template>
+
+<style lang="less">
+.nacos {
+  margin: 0;
+  min-height: 530px;
+  background: url("@/assets/image/term/nacos/nacos-bg.png");
+  padding-top: 200px;
+
+  .logo {
+    display: block;
+    width: 257px;
+    height: 50px;
+    margin: 0 auto;
+  }
+
+  .title {
+    opacity: 0.8;
+    font-family: Avenir-Medium;
+    font-size: 24px;
+    color: #fff;
+    max-width: 780px;
+    margin: 13px auto 30px;
+    text-align: center;
+  }
+}
+</style>
---
+++ src/views/term/Rabbit.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { outRabbit, toRabbit } from "@/api/out";
+
+onMounted(() => {
+  toRabbit();
+});
+</script>
+
+<template>
+  <div class="rabbit">
+    <img
+      @click="outRabbit()"
+      class="logo"
+      src="@/assets/image/term/rabbit/rabbit-logo.svg"
+      alt="rabbit"
+    />
+    <p class="title">A webinar on high availability and</p>
+    <p class="title">data safety in messaging</p>
+  </div>
+</template>
+
+<style lang="less">
+.rabbit {
+  margin: 0;
+  min-height: 530px;
+  background: url("@/assets/image/term/rabbit/rabbit-bg.svg") no-repeat 50px;
+  padding-top: 170px;
+
+  .logo {
+    display: block;
+    width: 400px;
+    // width: 257px;
+    // height: 50px;
+    margin-left: 70px;
+  }
+
+  .title {
+    opacity: 0.8;
+    font-family: Avenir-Medium;
+    font-size: 24px;
+    color: #000;
+    max-width: 780px;
+    margin-top: 30px;
+    margin-left: 70px;
+    text-align: left;
+  }
+}
+</style>
