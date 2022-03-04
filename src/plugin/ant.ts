ecbea40a1709a574ae532a55502b2a0fc9938740
john <tierney-john@hotmail.com>
2022-03-04 15:12:05 +0800

    ant 组件导入

--- src/plugin/ant.ts
+++ src/plugin/ant.ts
@@ -1,12 +1,31 @@
 import type { App } from "vue";
 
-import { Button, ConfigProvider, Layout, Menu, Result } from "ant-design-vue";
+import {
+  Button,
+  ConfigProvider,
+  Input,
+  Layout,
+  Menu,
+  PageHeader,
+  Pagination,
+  Result,
+  Spin,
+  Table,
+} from "ant-design-vue";
 
-import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons-vue";
-
-const components = [Button, ConfigProvider, Layout, Menu, Result];
-
-export const icons = [MenuFoldOutlined, MenuUnfoldOutlined];
+/** ant 组件全局引用 */
+const components = [
+  Button,
+  ConfigProvider,
+  Input,
+  Layout,
+  Menu,
+  PageHeader,
+  Pagination,
+  Result,
+  Spin,
+  Table,
+];
 
 export const useAntComponent = (app: App<Element>) => {
   components.forEach((component) => app.use(component));
