bd28f59e1f34a74b5398a1205701429fd3ed9307
john <tierney-john@hotmail.com>
2022-03-05 11:46:49 +0800

    用户信息保存

--- src/views/user/Info.vue
+++ src/views/user/Info.vue
@@ -1,18 +1,16 @@
-<!-- <script setup lang="ts">
-import TablePage, { type Column } from "@/components/page/TablePage.vue";
+<script setup lang="ts">
+import TablePage from "@/components/page/TablePage.vue";
 
 const title = "用户信息页";
 const subTitle = "用于添加和删除所属用户";
-</script> -->
+</script>
 
-<!--
 <template>
   <TablePage
     :title="title"
     :sub-title="subTitle"
-    :data="data" 
-    :columns="columns"
+    :data="[]"
+    :columns="[]"
     :loading="false"
   />
 </template>
--->
